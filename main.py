f892b525a7d4ada3a1e776e5d996a4dda9c08abc
Jefferson Silva Lopes <84870965+Jeffersonl22@users.noreply.github.com>
2022-06-14 17:47:56 -0300

    Avoiding invalid inputs and cleaning up the code

--- main.py
+++ main.py
@@ -7,12 +7,17 @@ def days_to_units(number_of_days):
         return f"{number_of_days} days are {number_of_days * calculate_to_hours} {name_of_units}"
     elif number_of_days == 0:
         return "You entered a 0, please enter a valid positive number!"
+
+
+def validate_and_execute():
+    if user_input.isdigit():
+        user_input_number = int(user_input)
+        calculated_value = days_to_units(user_input_number)
+        print(calculated_value)
     else:
-        return "You entered a negative value, so no conversion for you"
+        print("Your input is not a valid number!")
 
 
 user_input = input("Hey user, enter a number of days and I will convert to hours!\n")
-user_input_number = int(user_input)
+validate_and_execute()
 
-calculated_value = days_to_units(user_input_number)
-print(calculated_value)
