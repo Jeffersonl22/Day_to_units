08f126921df73160cfbf6238c5480caf2ec26aad
Jefferson Silva Lopes <84870965+Jeffersonl22@users.noreply.github.com>
2022-06-14 18:40:59 -0300

    Error Handling with Try / Except

--- main.py
+++ main.py
@@ -7,15 +7,17 @@ def days_to_units(number_of_days):
 
 
 def validate_and_execute():
-    if user_input.isdigit():
+    try:
+
         user_input_number = int(user_input)
         if user_input_number > 0:
             calculated_value = days_to_units(user_input_number)
             print(calculated_value)
         elif user_input_number == 0:
             print("You entered a 0, please enter a valid positive number!")
-
-    else:
+        else:
+            print("You entered a negative number, no conversion for you!")
+    except ValueError:
         print("Your input is not a valid number!")
 
 
