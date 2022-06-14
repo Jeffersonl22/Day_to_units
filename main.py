35bc225e1297d9033671f6e15e8819ef3b3f1017
Jefferson Silva Lopes <84870965+Jeffersonl22@users.noreply.github.com>
2022-06-14 17:59:09 -0300

    Nested if...else

--- main.py
+++ main.py
@@ -3,21 +3,21 @@ calculate_to_hours = 24
 
 
 def days_to_units(number_of_days):
-    if number_of_days > 0:
-        return f"{number_of_days} days are {number_of_days * calculate_to_hours} {name_of_units}"
-    elif number_of_days == 0:
-        return "You entered a 0, please enter a valid positive number!"
+    return f"{number_of_days} days are {number_of_days * calculate_to_hours} {name_of_units}"
 
 
 def validate_and_execute():
     if user_input.isdigit():
         user_input_number = int(user_input)
-        calculated_value = days_to_units(user_input_number)
-        print(calculated_value)
+        if user_input_number > 0:
+            calculated_value = days_to_units(user_input_number)
+            print(calculated_value)
+        elif user_input_number == 0:
+            print("You entered a 0, please enter a valid positive number!")
+
     else:
         print("Your input is not a valid number!")
 
 
 user_input = input("Hey user, enter a number of days and I will convert to hours!\n")
 validate_and_execute()
-
