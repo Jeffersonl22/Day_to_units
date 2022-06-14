cc613eebabaf76bb263b8fd485793eeb0cc74645
Jefferson Silva Lopes <84870965+Jeffersonl22@users.noreply.github.com>
2022-06-14 17:35:59 -0300

    Testing if & else in Python

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ calculate_to_hours = 24
 def days_to_units(number_of_days):
     if number_of_days > 0:
         return f"{number_of_days} days are {number_of_days * calculate_to_hours} {name_of_units}"
+    elif number_of_days == 0:
+        return "You entered a 0, please enter a valid positive number!"
     else:
         return "You entered a negative value, so no conversion for you"
 
