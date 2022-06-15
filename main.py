731252590044d29364fbedf887acb844d247eb62
Jefferson Silva Lopes <84870965+Jeffersonl22@users.noreply.github.com>
2022-06-14 21:28:10 -0300

    Update main.py

--- main.py
+++ main.py
@@ -27,9 +27,8 @@ def validate_and_execute():
 
 user_input = ""
 while user_input != "exit":
-    user_input = input("Hey user, enter a number of days and conversion unit\n")
+    user_input = input("Hey user, enter a number of days and conversion unit separate by :\n"
+                       "(You can also tap exit for end the program)\n")
     days_and_unit = user_input.split(":")
-    print(days_and_unit)
     days_and_unit_dictionary = {"days": days_and_unit[0], "unit": days_and_unit[1]}
-    print(days_and_unit_dictionary)
     validate_and_execute()
